d4221213317d722637dcc068eacdb62be8b20f9f
Miha Bondarenko <limon18@ukr.net>
2022-09-13 23:59:32 +0300

    add Task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery')
+galleryRef.addEventListener('click', onImgClick);
+galleryRef.addEventListener('keydown',offImgPush)
 
+  const listGalleryEl = galleryItems.map(({preview, original, description}) => {
+    return `
+      <div class="gallety__item">
+        <a href="${original}" class="gallery__link">
+          <img src="${preview}" data-source="${original}" alt="${description}" width="315" height="213" class="gallery__image">
+        </a>
+      </div>`;
+  });
+  let markup = listGalleryEl.join('');
+  galleryRef.insertAdjacentHTML('afterbegin', markup)
+
+  function onImgClick(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') {
+      return;
+    };
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+  `);
+    instance.show();
+  };
+
+  function offImgPush(evt) {
+    if(evt.code === 'Escape'){
+      instance.close()
+    };
+  };
 //todo Завдання 1 - галерея зображень
 //! Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
 //? Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
